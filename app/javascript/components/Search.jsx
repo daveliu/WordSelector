7f58e94a8694705fea1581615121c3aeaa37e8b3
Dave <dave@liageren.com>
2018-12-27 10:37:10 +0800

    add WAIT_INTERVAL for stop typing

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -2,24 +2,40 @@ import React from 'react';
 import axios from 'axios';
 import Autocomplete from 'react-autocomplete'
 
+const WAIT_INTERVAL = 1000;
+const ENTER_KEY = 13;
 
 export default class Search extends React.Component {
-  state = { value: '', results: [], mywords: [] };
+  state = { value: '', results: [], mywords: [] }
 
   constructor(props) {
     super(props)
+    this.triggerChange = this.triggerChange.bind(this)
+  }
+
+  componentWillMount() {
+    this.timer = null
+
     axios.get('/mywords.json', { params: {} })
       .then(res => this.setState({ mywords: res.data }))
   }
 
+  // Input fire onChange when user stopped typing
   onChange = (e, value) => {
+    clearTimeout(this.timer)
     this.setState({ value })
+    this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
+  }
+
+  triggerChange() {
+    const value = this.state.value
+
     if (value.length > 0) {
       axios.get('/search.json', { params: { query: value } })
         .then(res => this.setState({ results: res.data }))
-        .catch(() => this.setState({ results: [] }));
+        .catch(() => this.setState({ results: [] }))
     } else {
-      this.setState({ results: [] });
+      this.setState({ results: [] })
     }
   }
 
@@ -32,7 +48,7 @@ export default class Search extends React.Component {
   }
 
   render() {
-    const { loading, results, mywords } = this.state;
+    const { value, results, mywords } = this.state
     return (
       <div class="middle aligned row">
         <div class="ui container">
